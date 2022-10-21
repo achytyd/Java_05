7f77d93d75fdb401e179f62f588cd4d5c484be18
ach <idartay@gmail.com>
2022-10-21 13:13:51 -0700

    Added OddIndices

---
+++ src/main/java/OddEven.java
@@ -0,0 +1,5 @@
+public class OddEven {
+    public static oddEven();
+
+
+}
---
+++ src/main/java/project_utils/Utils.java
@@ -0,0 +1,8 @@
+package project_utils;
+
+public class Utils {
+    // keep all universal (can help to solve any tasks) methods here
+
+
+
+}
